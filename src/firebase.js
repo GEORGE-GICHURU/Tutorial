34ed1b2e7f15947a38cc93c4ed2450bb8f87141c
GEORGE-GICHURU <gichurugeorge092@gmail.com>
2023-03-12 15:20:58 +0300

    sidebar + scss

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_FIREBASE_KEY,
+//   authDomain: "tutorial-f700c.firebaseapp.com",
+//   projectId: "tutorial-f700c",
+//   storageBucket: "tutorial-f700c.appspot.com",
+//   messagingSenderId: "930245805223",
+//   appId: "1:930245805223:web:7b1a7ea4df996bd7e64782"
+// };
+
+// // Initialize Firebase
+// // const app = initializeApp(firebaseConfig);
