90a66b3eef854d6487bc3bdb520421fa803a1580
GEORGE-GICHURU <gichurugeorge092@gmail.com>
2023-03-14 21:15:47 +0300

    Firebase storage

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,21 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_FIREBASE_KEY,
-//   authDomain: "tutorial-f700c.firebaseapp.com",
-//   projectId: "tutorial-f700c",
-//   storageBucket: "tutorial-f700c.appspot.com",
-//   messagingSenderId: "930245805223",
-//   appId: "1:930245805223:web:7b1a7ea4df996bd7e64782"
-// };
 
-// // Initialize Firebase
-// // const app = initializeApp(firebaseConfig);
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: "tutorial-f700c.firebaseapp.com",
+  projectId: "tutorial-f700c",
+  storageBucket: "tutorial-f700c.appspot.com",
+  messagingSenderId: "930245805223",
+  appId: "1:930245805223:web:7b1a7ea4df996bd7e64782"
+};
+
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth();
+export const storage = getStorage(app);
